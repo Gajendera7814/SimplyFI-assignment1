4e9ac147aa40f3f873db57d1c21bf5fd30a1c1e2
Gajendera <gajenderakumar806@gmail.com>
2022-02-11 00:30:25 +0530

    commit

--- src/component/UserData.js
+++ src/component/UserData.js
@@ -16,21 +16,19 @@ function UserData() {
     return (
         <div className="App" id="parentDiv">
             {data.map((item, i) => (
-                <>
-                <div className="card" key={i}>
-                <div id="imgDiv">
-                    <img id="img" src={`https://avatars.dicebear.com/v2/avataaars/{${item.username}}.svg?options[mood][]=happy`} />
+                    <div className="card" key={i}>
+                        <div id="imgDiv" key={`${i}+b`}>
+                            <img id="img" src={`https://avatars.dicebear.com/v2/avataaars/{${item.username}}.svg?options[mood][]=happy`} />
+                        </div>
+                        <div id="detailsDiv" key={`${i}+a`}>
+                            <h1>{item.name}</h1>
+                            <p><strong>Email:</strong> {item.email} </p>
+                            <p><strong>Phone:</strong> {item.phone} </p>
+                            <p><strong>Company:</strong> {item.company.name} </p>
+                            <p><strong>Website:</strong> {item.website} </p>
+                            <p><strong>Address:</strong> {item.address.street}, {item.address.suite}, {item.address.city}, {item.address.zipcode} </p>
+                        </div>
                     </div>
-                    <div id="detailsDiv">
-                    <h1>{item.name}</h1>
-                    <p><strong>Email:</strong> {item.email} </p>
-                    <p><strong>Phone:</strong> {item.phone} </p>
-                    <p><strong>Company:</strong> {item.company.name} </p>
-                    <p><strong>Website:</strong> {item.website} </p>
-                    <p><strong>Address:</strong> {item.address.street}, {item.address.suite}, {item.address.city}, {item.address.zipcode} </p>
-                    </div>                  
-                </div>                   
-                </>
             ))}
         </div>
     );
