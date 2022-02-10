f91e6cbec2a181a8a05888b2f073c4f57861f32c
Gajendera <gajenderakumar806@gmail.com>
2022-02-10 23:25:11 +0530

    initial commit

---
+++ src/component/UserData.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import '../App.css'
+
+function UserData() {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users").then((result) => {
+            result.json().then((resp) => {
+                console.log(resp)
+                setData(resp);
+            });
+        });
+    }, []);
+
+
+    return (
+        <div className="App" id="parentDiv">
+            {data.map((item, i) => (
+                <>
+                <div className="card" key={i}>
+                <div id="imgDiv">
+                    <img id="img" src={`https://avatars.dicebear.com/v2/avataaars/{${item.username}}.svg?options[mood][]=happy`} />
+                    </div>
+                    <div id="detailsDiv">
+                    <h1>{item.name}</h1>
+                    <p><strong>Email:</strong> {item.email} </p>
+                    <p><strong>Phone:</strong> {item.phone} </p>
+                    <p><strong>Company:</strong> {item.company.name} </p>
+                    <p><strong>Website:</strong> {item.website} </p>
+                    <p><strong>Address:</strong> {item.address.street}, {item.address.suite}, {item.address.city}, {item.address.zipcode} </p>
+                    </div>                  
+                </div>                   
+                </>
+            ))}
+        </div>
+    );
+}
+export default UserData;
